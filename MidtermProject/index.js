445453fe7067266d230e962ba71455224a4a850a
anbu <anbu@iastate.edu>
2023-03-10 16:23:56 -0600

    Made it so Reviews pop up under images reviews pg

--- MidtermProject/index.js
+++ MidtermProject/index.js
@@ -24,46 +24,49 @@ fetch('./data.json')
           </div>
         </div>
       `;
-       // Add the movie card to the movies container
-       moviesContainer.insertAdjacentHTML('beforeend', movieCard);
+      // Add the movie card to the movies container
+      moviesContainer.insertAdjacentHTML('beforeend', movieCard);
 
-       // Add event listener to image element
-       const image = moviesContainer.querySelector(`img[alt="${movie.movieId}"]`);
-       image.addEventListener('mouseenter', () => {
-         // Change cursor to hand
-         image.style.cursor = 'pointer';
-       });
-       image.addEventListener('mouseleave', () => {
-         // Change cursor back to default
-         image.style.cursor = 'default';
-       });
-       image.addEventListener('click', () => {
-         // Redirect to review page
-         window.location.href = `./index2.html`;
-       });
-     });
-   });
+      // Add event listener to image element
+      const image = moviesContainer.querySelector(`img[alt="${movie.movieId}"]`);
+      image.addEventListener('mouseenter', () => {
+        // Change cursor to hand
+        image.style.cursor = 'pointer';
+      });
+      image.addEventListener('mouseleave', () => {
+        // Change cursor back to default
+        image.style.cursor = 'default';
+      });
+      image.addEventListener('click', () => {
+        // Redirect to review page
+        window.location.href = `./index2.html`;
+      });
+    });
+  });
 
-   const moviesReviewContainer = document.getElementById('reviews');
 
-   fetch('./data.json')
-    .then(response => response.json())
-    .then(data => {
-      data.forEach(review => {
-        const reviewCard = `
-        <div class = "col">
-          <div class = "card shadow-sm">
-           <img src ="${review.imageSrc}" alt = "${review.movieId}">
-           <div class ="card-body">
-            <p class = "card-text">${movie.review}</p>
-            <div class ="d-flex justify-content-between align-items-center">
-              <div class = "btn-group">
+const moviesReviewContainer = document.getElementById('reviews');
+
+fetch('./data.json')
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(review => {
+      const reviewCard = `
+        <div class="col">
+          <div class="card shadow-sm">
+            <img src="${review.imageSrc}" alt="${review.movieId}">
+            <div class="card-body">
+              <p class="card-text">${review.review} ${review.reviewAuthor}</p>
+              <p class="card-text">${review.review2} ${review.review2Author}</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                </div>
               </div>
             </div>
           </div>
         </div>
       `;
-        moviesReviewContainer.insertAdjacentHTML('beforeend', reviewCard);
-        const image = moviesReviewContainer.querySelector(`img[alt ="${movie.movieId}"]`);
-      });
+      moviesReviewContainer.insertAdjacentHTML('beforeend', reviewCard);
+      const image = moviesReviewContainer.querySelector(`img[alt="${review.movieId}"]`);
     });
+  });
