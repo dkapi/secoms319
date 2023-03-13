39cc2d68a13b44ece57cfaaf54df8b359f8ae97c
anbu <anbu@iastate.edu>
2023-03-12 19:50:22 -0500

    final

--- MidtermProject/index.js
+++ MidtermProject/index.js
@@ -1,3 +1,44 @@
+const header = document.createElement('header');
+header.innerHTML = `
+  <div class="collapse bg-dark" id="navbarHeader">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-8 col-md-7 py-4">
+          <h2 class="text-white">An Assortment of Movies</h2>
+          <h4 class="text-white">We hope you enjoy!</h4>
+        </div>
+        <div class="col-sm-4 offset-md-1 py-4">
+          <h4 class="text-white">Image Sources</h4>
+          <ul>
+          <li><a href="https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg">
+            Avengers Movie Poster</a>
+          </li>
+          <li><a href="https://c4.wallpaperflare.com/wallpaper/764/590/391/inception-leonardo-dicaprio-movie-posters-2400x3500-entertainment-movies-hd-art-wallpaper-preview.jpg">
+            Inception Movie Poster</a>
+          </li>
+          <li><a href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSp6Py6zIThAfLViyfCFHT-yfMZ7gFY0imj8WBddOUQfZzAWDYh">
+            Scarface Movie Poster</a>
+          </li>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="navbar navbar-dark bg-dark shadow-sm">
+    <div class="container">
+      <a href="#" class="navbar-brand d-flex align-items-center">
+        <form action="./index.html"><input type="submit" value="Movies" class="formnavbar"></form>
+        <form action="./index2.html"><input type="submit" value="Reviews" class="formnavbar"></form>
+        <form action="./about.html"><input type="submit" value="About Us" class="formnavbar"></form>
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    </div>
+  </div>
+`
+const headerContainer = document.getElementById('header-container');
+headerContainer.appendChild(header);;
+
 const moviesContainer = document.getElementById('movies');
 
 
@@ -24,6 +65,7 @@ fetch('./data.json')
           </div>
         </div>
       `;
+      
       // Add the movie card to the movies container
       moviesContainer.insertAdjacentHTML('beforeend', movieCard);
 
@@ -47,6 +89,35 @@ fetch('./data.json')
 
 const moviesReviewContainer = document.getElementById('reviews');
 
+// function to display larger image
+function showLargerImage(event) {
+  // create larger image element
+  const largerImage = document.createElement('img');
+  largerImage.src = event.target.src;
+  largerImage.style.maxHeight = '80%';
+
+  // create overlay element
+  const overlay = document.createElement('div');
+  overlay.style.position = 'fixed';
+  overlay.style.top = '0';
+  overlay.style.left = '0';
+  overlay.style.width = '100%';
+  overlay.style.height = '100%';
+  overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.8)';
+  overlay.style.display = 'flex';
+  overlay.style.justifyContent = 'center';
+  overlay.style.alignItems = 'center';
+  overlay.appendChild(largerImage);
+
+  // append overlay to body
+  document.body.appendChild(overlay);
+
+  // add click event listener to overlay to remove it when clicked
+  overlay.addEventListener('click', () => {
+    document.body.removeChild(overlay);
+  });
+}
+
 fetch('./data.json')
   .then(response => response.json())
   .then(data => {
@@ -56,9 +127,12 @@ fetch('./data.json')
           <div class="card shadow-sm">
             <img src="${review.imageSrc}" alt="${review.movieId}">
             <div class="card-body">
+            <div class = "review-text">
               <p class="card-text">${review.review} ${review.reviewAuthor}</p>
               <p class="card-text">${review.review2} ${review.review2Author}</p>
+              </div>
               <div class="d-flex justify-content-between align-items-center">
+              </div>
                 <div class="btn-group">
                 </div>
               </div>
@@ -68,5 +142,19 @@ fetch('./data.json')
       `;
       moviesReviewContainer.insertAdjacentHTML('beforeend', reviewCard);
       const image = moviesReviewContainer.querySelector(`img[alt="${review.movieId}"]`);
-    }); 
+      image.addEventListener('click', showLargerImage);
+
+      const reviewTextElements = moviesReviewContainer.querySelectorAll('.review-text');
+
+      reviewTextElements.forEach(reviewText => {
+        reviewText.addEventListener('click', () => {
+          reviewText.classList.toggle('expanded');
+          reviewText.classList.toggle('bold');
+        });
+      });
+
+    });
   });
+
+
+
